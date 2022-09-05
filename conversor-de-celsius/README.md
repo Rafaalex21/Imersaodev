8f8300979afd28f4704e6e3fcb48e5413280ccf9
Rafaalex21 <95985790+Rafaalex21@users.noreply.github.com>
2022-09-05 18:50:47 -0300

    Add files via upload

---
+++ conversor-de-celsius/README.md
@@ -0,0 +1,4 @@
+# Conversor de Celsius
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Rafaalex21/pen/KKRpddQ](https://codepen.io/Rafaalex21/pen/KKRpddQ).
+
---
+++ conversor-de-celsius/dist/script.js
@@ -0,0 +1,5 @@
+var tempC = parseInt(prompt("Coloque um valor de temperatura em Celsius"))
+
+
+document.getElementById("resultado").innerText = "O valor de " + (tempC) + " C em Farenheit é " + (tempC  *1.8 +32 ) + "F"
+// *1.8 +32
