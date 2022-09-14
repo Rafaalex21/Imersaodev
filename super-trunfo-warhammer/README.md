e3130b4ba4e7163b2d4a2585202596e46e86b069
Rafaalex21 <95985790+Rafaalex21@users.noreply.github.com>
2022-09-14 18:30:19 -0300

    Add files via upload

---
+++ super-trunfo-warhammer/README.md
@@ -0,0 +1,4 @@
+# Super Trunfo Warhammer
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Rafaalex21/pen/LYmbaVZ](https://codepen.io/Rafaalex21/pen/LYmbaVZ).
+
---
+++ super-trunfo-warhammer/src/script.js
@@ -0,0 +1,327 @@
+var carta1 = {
+  nome: "Lion El'Johnson ",
+  imagem:
+    "https://i.pinimg.com/originals/d9/83/24/d9832446986e66a2e25aaa18d63c9c1c.jpg",
+  atributos: {
+    ataque: 9,
+    defesa: 7,
+    magia: 1
+  }
+};
+
+var carta2 = {
+  nome: "Fulgrim",
+  imagem:
+    "https://wh40k.lexicanum.com/mediawiki/images/thumb/b/be/FulgrimArtNew.jpg/300px-FulgrimArtNew.jpg",
+  atributos: {
+    ataque: 8,
+    defesa: 5,
+    magia: 3
+  }
+};
+
+var carta3 = {
+  nome: "Perturabo",
+  imagem:
+    "https://i.pinimg.com/originals/f0/af/45/f0af45c30cc0344b70231a5531d37b32.jpg",
+  atributos: {
+    ataque: 6,
+    defesa: 8,
+    magia: 1
+  }
+};
+var carta4 = {
+  nome: "Jaghatai Khan",
+  imagem:
+    "https://cdnb.artstation.com/p/assets/covers/images/042/775/565/large/vitaly-perevoshikov-vitaly-perevoshikov-5.jpg?1635405061",
+  atributos: {
+    ataque: 8,
+    defesa: 5,
+    magia: 2
+  }
+};
+var carta5 = {
+  nome: "Leman Russ",
+  imagem:
+    "https://www.wargamer.com/wp-content/sites/wargamer/2022/07/warhammer-40k-leman-russ-the-great-wolf.jpg",
+  atributos: {
+    ataque: 7,
+    defesa: 6,
+    magia: 1
+  }
+};
+var carta6 = {
+  nome: "Rogal Dorn",
+  imagem:
+    "https://i.pinimg.com/736x/06/78/ff/0678ffe086eb35139a4ea56d9f669b1f.jpg",
+  atributos: {
+    ataque: 8,
+    defesa: 8,
+    magia: 3
+  }
+};
+var carta7 = {
+  nome: "Konrad Curze",
+  imagem:
+    "https://i.pinimg.com/474x/f2/65/b7/f265b79ca2479f9c5f92beca0b9a0f1d.jpg",
+  atributos: {
+    ataque: 10,
+    defesa: 1,
+    magia: 2
+  }
+};
+var carta8 = {
+  nome: "Sanguinius",
+  imagem:
+    "https://spikeybits.com/wp-content/uploads/2017/11/Sanguinius-in-battle.jpg",
+  atributos: {
+    ataque: 10,
+    defesa: 5,
+    magia: 9
+  }
+};
+var carta9 = {
+  nome: "Ferrus Manus",
+  imagem:
+    "https://cs14.pikabu.ru/post_img/2022/09/06/0/1662412837160981173.jpg",
+  atributos: {
+    ataque: 6,
+    defesa: 5,
+    magia: 3
+  }
+};
+var carta10 = {
+  nome: "Angron",
+  imagem:
+    "https://www.warhammer-community.com/wp-content/uploads/2022/07/jZjrnFZoJe5e0oBs.jpg",
+  atributos: {
+    ataque: 8,
+    defesa: 7,
+    magia: 1
+  }
+};
+var carta11 = {
+  nome: "Roboute Guilliman",
+  imagem: "https://img-9gag-fun.9cache.com/photo/a7WQP9b_460s.jpg",
+  atributos: {
+    ataque: 7,
+    defesa: 6,
+    magia: 3
+  }
+};
+var carta12 = {
+  nome: "Mortarion",
+  imagem:
+    "https://i.pinimg.com/736x/22/df/8c/22df8c614c7fa2e45e4a059c3121cee6.jpg",
+  atributos: {
+    ataque: 7,
+    defesa: 10,
+    magia: 6
+  }
+};
+var carta13 = {
+  nome: "Magnus",
+  imagem:
+    "https://i.pinimg.com/originals/63/e2/e2/63e2e227bbc6c34f70b3160acd508e4d.jpg",
+  atributos: {
+    ataque: 5,
+    defesa: 5,
+    magia: 10
+  }
+};
+var carta14 = {
+  nome: "Horus Lupercal",
+  imagem:
+    "https://www.wargamer.com/wp-content/uploads/2022/06/warhammer-40k-horus-lupercal-900x506.jpg",
+  atributos: {
+    ataque: 9,
+    defesa: 9,
+    magia: 9
+  }
+};
+var carta15 = {
+  nome: "Lorgar",
+  imagem:
+    "https://64.media.tumblr.com/c697f6a1520fbd80c169cef21fb1041b/976f2161f452dc66-9a/s1280x1920/e57d4587f99bd190f4b5741604378654b2a2d66e.jpg",
+  atributos: {
+    ataque: 5,
+    defesa: 5,
+    magia: 5
+  }
+};
+var carta16 = {
+  nome: "Vulkan",
+  imagem:
+    "https://i.pinimg.com/originals/b5/a9/3a/b5a93a0383fe0e51dfa420184c112b55.jpg",
+  atributos: {
+    ataque: 9,
+    defesa: 10,
+    magia: 4
+  }
+};
+var carta17 = {
+  nome: "Corvus Corax",
+  imagem:
+    "https://cdnb.artstation.com/p/assets/images/images/045/707/071/large/artur-nakhodkin-corvus-corax-artstation.jpg?1643324905",
+  atributos: {
+    ataque: 9,
+    defesa: 7,
+    magia: 3
+  }
+};
+var carta18 = {
+  nome: "Alpharius Omegon",
+  imagem:
+    "https://cdna.artstation.com/p/assets/images/images/047/046/378/large/johannes-helgeson-fnfhlfcxeamy8ei.jpg?1646642060",
+  atributos: {
+    ataque: 7,
+    defesa: 7,
+    magia: 7
+  }
+};
+var carta19 = {
+  nome: "Imperador da Humanidade",
+  imagem:
+    "https://i.pinimg.com/originals/1f/29/b8/1f29b8fe27c169d9799a08ab6f6bc1d3.jpg",
+  atributos: {
+    ataque: 10,
+    defesa: 10,
+    magia: 10
+  }
+};
+var carta20 = {
+  nome: "Sly marbo",
+  imagem:
+    "https://cdn.vrcmods.com/uploads/items/item/3688/imgs/6e14414deee04d4e9563e54b703c3c30.jpg",
+  atributos: {
+    ataque: 2000,
+    defesa: 2000,
+    magia: 0
+  }
+};
+
+var cartaMaquina;
+var cartaJogador;
+var cartas = [
+  carta1,
+  carta2,
+  carta3,
+  carta4,
+  carta5,
+  carta6,
+  carta7,
+  carta8,
+  carta9,
+  carta10,
+  carta11,
+  carta12,
+  carta13,
+  carta14,
+  carta15,
+  carta16,
+  carta17,
+  carta18,
+  carta19,
+  carta20
+];
+// 0          1           2
+
+function sortearCarta() {
+  var numeroCartaMaquina = parseInt(Math.random() * 20);
+  cartaMaquina = cartas[numeroCartaMaquina];
+
+  var numeroCartaJogador = parseInt(Math.random() * 20);
+  while (numeroCartaJogador == numeroCartaMaquina) {
+    numeroCartaJogador = parseInt(Math.random() * 20);
+  }
+  cartaJogador = cartas[numeroCartaJogador];
+  console.log(cartaJogador);
+
+  document.getElementById("btnSortear").disabled = true;
+  document.getElementById("btnJogar").disabled = false;
+  exibirCartaJogador();
+}
+
+function obtemAtributoSelecionado() {
+  var radioAtributo = document.getElementsByName("atributo");
+  for (var i = 0; i < radioAtributo.length; i++) {
+    if (radioAtributo[i].checked) {
+      return radioAtributo[i].value;
+    }
+  }
+}
+
+function jogar() {
+  console.log("chamou");
+  var atributoSelecionado = obtemAtributoSelecionado();
+  var divResultado = document.getElementById("resultado");
+  if (atributoSelecionado == undefined) {
+    return;
+  }
+
+  if (
+    cartaJogador.atributos[atributoSelecionado] >
+    cartaMaquina.atributos[atributoSelecionado]
+  ) {
+    htmlResultado = "<p class='resultado-final'>A vitória é sua!</p>";
+  } else if (
+    cartaJogador.atributos[atributoSelecionado] <
+    cartaMaquina.atributos[atributoSelecionado]
+  ) {
+    htmlResultado = "<p class='resultado-final'>Você foi derrotado!</p>";
+  } else {
+    htmlResultado = "<p class='resultado-final'>Empate</p>";
+  }
+  divResultado.innerHTML = htmlResultado;
+
+  document.getElementById("btnJogar").disabled = true;
+  exibirCartaMaquina();
+}
+
+function exibirCartaJogador() {
+  var divCartaJogador = document.getElementById("carta-jogador");
+  divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`;
+  // divCartaJogador.style.backgroundImage = "url(" + cartaJogador.imagem + ")"
+  var moldura =
+    '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+  var tagHTML = "<div id='opcoes' class='carta-status'>";
+
+  var opcoesTexto = "";
+  for (var atributo in cartaJogador.atributos) {
+    opcoesTexto +=
+      "<input type='radio' name='atributo' value='" +
+      atributo +
+      "'>" +
+      atributo +
+      " " +
+      cartaJogador.atributos[atributo] +
+      "<br>";
+  }
+  var nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>`;
+
+  divCartaJogador.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
+}
+
+function exibirCartaMaquina() {
+  var divCartaMaquina = document.getElementById("carta-maquina");
+  divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.imagem})`;
+  // divCartaJogador.style.backgroundImage = "url(" + cartaJogador.imagem + ")"
+  var moldura =
+    '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+  var tagHTML = "<div id='opcoes' class='carta-status'>";
+
+  var opcoesTexto = "";
+  for (var atributo in cartaMaquina.atributos) {
+    opcoesTexto +=
+      "<p type='text' name='atributo' value='" +
+      atributo +
+      "'>" +
+      atributo +
+      " " +
+      cartaMaquina.atributos[atributo] +
+      "</p>";
+  }
+  var nome = `<p class="carta-subtitle">${cartaMaquina.nome}</p>`;
+
+  divCartaMaquina.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
+}
